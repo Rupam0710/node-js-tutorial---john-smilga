8481d32e2b627ae66639223cf2cdc78d0230e63b
Rupam <rupampal48@gmail.com>
2023-09-14 01:23:22 +0530

    9th com - fs module(sync & async)

---
+++ 11.fs_async_module.js
@@ -0,0 +1,27 @@
+const { readFile, writeFile } = require("fs");
+
+readFile("./context/1st.txt", "utf-8", (err, result) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  const first = result;
+  readFile("./context/2nd.txt", "utf-8", (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const second = result;
+    writeFile(
+      "./context/result_file_async.txt",
+      `Hello this is the result : ${first},${second}`,
+      (err, result) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log(result);
+      }
+    );
+  });
+});
