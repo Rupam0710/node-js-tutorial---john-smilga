16381426404c7dfb50c42f2435f667fccedf8664
Rupam <rupampal48@gmail.com>
2023-09-15 00:46:10 +0530

    12th com - http more features

--- 12.http_module_setup.js
+++ 12.http_module_setup.js
@@ -1,8 +1,17 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.write(`Welcome to my server`);
-  res.end();
+  if (req.url === "/") {
+    res.end(`Welcome to my server`);
+  }
+  if (req.url === "/about") {
+    res.end(`Welcome to my about page`);
+  }
+  res.end(`
+    <h1>Oops!!</h1>
+    <p>We can't find the page you are looking for</p>
+    <a href='/'>Back Home</a>
+    `);
 });
 
 server.listen(5000);
