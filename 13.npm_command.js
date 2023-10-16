47ae1fdd849344601d9b84afb9d6eee684ceb295
Rupam <rupampal48@gmail.com>
2023-10-16 21:48:58 +0530

    13th com - npm_command

---
+++ 13.npm_command.js
@@ -0,0 +1,9 @@
+// npm - global command, come with node
+// npm --version
+
+//local dependency - use it only in this particular project
+// npm i <packageName>
+
+//global dependency - use it any project
+// npm install -g <packageName>
+// sudo npm install -g <packageName> (mac)
